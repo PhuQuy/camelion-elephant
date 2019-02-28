32e0a092b1178984e279800f656006d4609168df
PhuQuy <phuquy.tool@gmail.com>
2019-02-28 11:05:49 +0700

    update

--- src/app/layouts/clients/clients.module.ts
+++ src/app/layouts/clients/clients.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientsComponent } from './clients.component';
 import { ClientsRoutingModule } from './clients.routing';
+import { NavbarModule } from '@components/navbar/navbar.module';
+import { ShareModule } from '@shared/share/share.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ClientsRoutingModule
+    ClientsRoutingModule,
+    NavbarModule,
+    ShareModule
   ],
   declarations: [ClientsComponent],
   exports: [ClientsRoutingModule]
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, AfterViewInit, ViewChild, ElementRef, PLATFORM_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
 
 declare let $:any;
 @Component({
@@ -8,8 +8,6 @@ declare let $:any;
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements AfterViewInit {
-    @ViewChild('rippleAnimation') rippleAnimation: ElementRef;
-
     constructor(@Inject(PLATFORM_ID) public platformId: string) {
         
     }
@@ -18,9 +16,66 @@ export class HomeComponent implements AfterViewInit {
         if (isPlatformBrowser(this.platformId)) {
             $('.hero-area-fix').ripples({
                 resolution: 712,
-                dropRadius: 20,
-                perturbance: 0.04,
+                dropRadius: 40,
+                perturbance: 0.08,
             });
+
+            var TxtType = function(el, toRotate, period) {
+                this.toRotate = toRotate;
+                this.el = el;
+                this.loopNum = 0;
+                this.period = parseInt(period, 10) || 2000;
+                this.txt = '';
+                this.tick();
+                this.isDeleting = false;
+            };
+        
+            TxtType.prototype.tick = function() {
+                var i = this.loopNum % this.toRotate.length;
+                var fullTxt = this.toRotate[i];
+        
+                if (this.isDeleting) {
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+                } else {
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+                }
+        
+                this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+        
+                var that = this;
+                var delta = 200 - Math.random() * 100;
+        
+                if (this.isDeleting) { delta /= 2; }
+        
+                if (!this.isDeleting && this.txt === fullTxt) {
+                delta = this.period;
+                this.isDeleting = true;
+                } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                this.loopNum++;
+                delta = 500;
+                }
+        
+                setTimeout(function() {
+                that.tick();
+                }, delta);
+            };
+        
+            window.onload = function() {
+                var elements = document.getElementsByClassName('typewrite');
+                for (var i=0; i<elements.length; i++) {
+                    var toRotate = elements[i].getAttribute('data-type');
+                    var period = elements[i].getAttribute('data-period');
+                    if (toRotate) {
+                      new TxtType(elements[i], JSON.parse(toRotate), period);
+                    }
+                }
+                // INJECT CSS
+                var css = document.createElement("style");
+                css.type = "text/css";
+                css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+                document.body.appendChild(css);
+            };
             
         }
     }
