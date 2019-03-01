c74721ba6ea1446d954b3b542305e9daf1d3ab03
PhuQuy <phuquy.tool@gmail.com>
2019-03-01 16:27:12 +0700

    update logo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,12 +9,18 @@ import { isPlatformBrowser } from '@angular/common';
 export class NavbarComponent implements OnInit {
     scrolled: boolean = false;
     awake: boolean = false;
+    logo: string = '/assets/images/logo3.png';
 
     @HostListener("window:scroll", [])
     onWindowScroll() {
         if (isPlatformBrowser(this.platformId)) {
             this.scrolled = window.scrollY > 60;
             this.awake = window.scrollY > 100;
+            if (window.scrollY > 100) {
+                this.logo = "/assets/images/logo4.png";
+            } else {
+                this.logo = '/assets/images/logo3.png';
+            }
         }
     }
 
