059adb4f784555ecab44e72bad7f8e05519de222
lyhuynhduybao <lyhuynhduybao@gmail.com>
2019-05-07 16:50:33 +0700

    Change UI

--- src/app/routes/about/about.component.ts
+++ src/app/routes/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, PLATFORM_ID, Inject, ViewChild, ElementRef } from '@angular/core';
 import { BaseComponent } from '@core/base/base.component';
 import { SeoService } from '@shared/seo.service';
-import { ViewCache } from '@firebase/database/dist/src/core/view/ViewCache';
+import { SlidesOutputData } from 'ngx-owl-carousel-o';
 
 @Component({
     selector: 'about',
@@ -15,11 +15,13 @@ export class AboutComponent extends BaseComponent implements OnInit {
         touchDrag: true,
         pullDrag: false,
         dots: true,
+        center:true,
         margin:20,
         navSpeed: 700,
-        stagePadding: 50,
+        stagePadding: 90,
         nav:true,
-        navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+        navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+        navClass: ['owl-prev', 'owl-next'],
         responsiveClass: true,
         responsive: {
             0: {
@@ -38,10 +40,9 @@ export class AboutComponent extends BaseComponent implements OnInit {
             }
         },
         navElement: 'div',
-        navClass: ['owl-prev', 'owl-next'],
+        
         lazyLoad: true,
         lazyLoadEager: 2,
-        responsiveBaseElement: 'div'
     };
     slideStore = [
         {
@@ -86,9 +87,38 @@ export class AboutComponent extends BaseComponent implements OnInit {
             name: "Dennis Green",
             job: "Web Developer"
         },
+        {
+            src: "/assets/images/person_3.jpg",
+            alt: "image 3",
+            title: 'Person 3',
+            name: "Dennis Green",
+            job: "System Analytics"
+        },
+        {
+            src: "/assets/images/person_1.jpg",
+            alt: "image 1",
+            title: 'Person 1',
+            name: "Dennis Green",
+            job: "Maketing Manager"
+        },
+        {
+            src: "/assets/images/person_2.jpg",
+            alt: "image 2",
+            title: 'Person 2',
+            name: "Dennis Green",
+            job: "Web Developer"
+        },
+        {
+            src: "/assets/images/person_3.jpg",
+            alt: "image 3",
+            title: 'Person 3',
+            name: "Dennis Green",
+            job: "Web Developer"
+        },
 
     ]
-
+    activeSlides: SlidesOutputData;
+    slidesStore: any[];
     constructor(@Inject(PLATFORM_ID) public platformId: string, private seoService: SeoService) {
         super(platformId);
     }
@@ -107,4 +137,8 @@ export class AboutComponent extends BaseComponent implements OnInit {
         this.initView();
     }
 
+    getData(data: SlidesOutputData) {
+        this.activeSlides = data;
+        console.log(this.activeSlides);
+      }
 }
