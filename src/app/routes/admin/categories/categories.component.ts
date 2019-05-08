a1a1d26667998eb17c3d993614c9908a4b370cdf
nvkhanh1775 <nvkhanh1775@gmail.com>
2019-05-08 09:15:07 +0700

    Category CRUD

--- src/app/routes/admin/categories/categories.component.ts
+++ src/app/routes/admin/categories/categories.component.ts
@@ -2,29 +2,64 @@ import { Component, Inject } from "@angular/core";
 import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 import { CategoryModalComponent } from "./category-modal/category-modal.component";
 import { ConfirmModalComponent } from "@components/admin/confirm-modal/confirm-modal.component";
+import { CategoryService } from "app/services/category.service";
 
 @Component({
 	selector: "app-admin-categories",
 	templateUrl: "./categories.component.html",
-	styleUrls: ["./categories.component.scss"]
+	styleUrls: ["./categories.component.scss"],
+	providers: [CategoryService]
 })
 export class CategoriesComponent {
 	closeResult: string;
+	categories;
+	
+	constructor(private modalService: NgbModal, protected categoryService: CategoryService) {}
 
-	constructor(private modalService: NgbModal) {}
+	ngOnInit() {
+		this.getAll();
+	}
 
-	ngOnInit() {}
+	getAll() {
+		this.categoryService.getAll().subscribe(categories => {
+			this.categories = categories;
+		})
+	}
 
 	onAdd() {
 		const modalRef = this.modalService.open(CategoryModalComponent);
 		modalRef.componentInstance.new = true;
+		modalRef.result.then(result => {
+			if (result) {
+				this.categoryService.create(result).then(() => {
+					this.getAll();
+				});
+			}
+		})
 	}
-	onEdit(){
+
+	onEdit(category){
 		const modalRef = this.modalService.open(CategoryModalComponent);
 		modalRef.componentInstance.new = false;
+		modalRef.componentInstance.category = category;
+		modalRef.result.then(result => {
+			if (result) {
+				this.categoryService.update(result).then(() => {
+					this.getAll();
+				});
+			}
+		})
 	}
-	onDelete(){
+
+	onDelete(category){
 		const modalRef = this.modalService.open(ConfirmModalComponent);
 		modalRef.componentInstance.title = 'Category delete';
+		modalRef.result.then(result => {
+			if (result) {
+				this.categoryService.delete(category.id).then(() => {
+					this.getAll();
+				});
+			}
+		})
 	}
 }
