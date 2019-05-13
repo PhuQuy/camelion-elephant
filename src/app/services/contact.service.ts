bcb8e20ab7df1a3ebc659bf2953ed7173090b39c
lyhuynhduybao <lyhuynhduybao@gmail.com>
2019-05-13 10:56:02 +0700

    Merge

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -5,10 +5,6 @@ import { AngularFirestore } from 'angularfire2/firestore';
 @Injectable()
 export class ContactService extends BaseService{
   constructor(protected angularFireDatabase: AngularFirestore) { 
-<<<<<<< HEAD
     super(angularFireDatabase, 'Contact');
-=======
-    super(angularFireDatabase, 'Contacts');
->>>>>>> b3d24307ffedcb934179dbd90d41595884fb2c1e
   }
 }
