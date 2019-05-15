52b867c217c11bb61a6cc6516a7ffffc619d0f15
lethinh0510 <lethinh0510@gmail.com>
2019-05-15 14:19:42 +0700

    logout

--- src/app/components/admin/navbar/navbar.component.ts
+++ src/app/components/admin/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { DOCUMENT } from "@angular/common";
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
 	selector: "app-admin-navbar",
@@ -10,12 +11,19 @@ import { DOCUMENT } from "@angular/common";
 export class NavbarComponent implements OnInit {
 	constructor(
 		private router: Router,
-		@Inject(DOCUMENT) private document: Document
+		@Inject(DOCUMENT) private document: Document,
+		 private firebaseAuth: AngularFireAuth
 	) {}
 
 	ngOnInit() {}
 
-	onToggleMenu(){
-		this.document.body.classList.toggle('is-collapsed');
+	onToggleMenu() {
+		this.document.body.classList.toggle("is-collapsed");
+	}
+
+	logout() {
+		this.firebaseAuth.auth.signOut().then(()=>{
+            this.router.navigate(['/login']);
+		});
 	}
 }
